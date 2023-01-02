726ad014b244d7f1ad58e43bd896fdd1b2f16425
Roger Urich <rogerdurich+git@gmail.com>
2023-01-01 19:06:17 -0800

    added cloudflare images and character model

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,14 @@
 export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-cloudflare-2",
+      providerOptions: {
+        accountId: env("accountId"),
+        apiKey: env("apiKey"),
+        variant: "sunny",
+      },
+    },
+  },
   "import-export-entries": {
     enabled: true,
   },
