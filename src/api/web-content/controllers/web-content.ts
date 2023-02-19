ef856d25fbc2c5fa15d7fda4b8237dbff2a34cd1
Roger Urich <rogerdurich@gmail.com>
2023-02-19 09:40:08 -0800

    added a bunch of types
    actors, genres, content blocks

---
+++ src/api/web-content/controllers/web-content.ts
@@ -0,0 +1,7 @@
+/**
+ * web-content controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::web-content.web-content');
---
+++ src/api/web-content/services/web-content.ts
@@ -0,0 +1,7 @@
+/**
+ * web-content service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::web-content.web-content');
