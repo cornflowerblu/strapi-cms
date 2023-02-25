255cc4407a64cc44b96e7f60712efbd48cd5a628
Roger Urich <rogerdurich@gmail.com>
2023-02-25 08:32:43 -0800

    fix secrets

--- config/admin.ts
+++ config/admin.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'admin-jwt-secret'),
+    secret: env('ADMIN_JWT_SECRET') || 'admin-jwt-secret',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'api-token-salt'),
+    salt: env('API_TOKEN_SALT') || 'api-token-salt',
   },
 });
